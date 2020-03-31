ab8db48330d5aebb02280d09494c040e9ca8bce1
mpohler1 <masonpohler@gmail.com>
2020-03-31 16:09:56 -0500

    Created components for general layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import './App.css';
+import AboutMe from "./components/AboutMe";
+import Testimonials from "./components/Testimonials";
+import Projects from "./components/Projects";
+import ContactMe from "./components/ContactMe";
+import Navbar from "./components/Navbar";
+import Headline from "./components/Headline";
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar/>
+      <Headline/>
+      <AboutMe/>
+      <Testimonials/>
+      <Projects/>
+      <ContactMe/>
     </div>
   );
 }
---
+++ src/components/Headline.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Headline extends Component {
+
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+export default Headline;
