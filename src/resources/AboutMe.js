ea95b778ceaaa5f8788a4cd871de7508f6a1c35b
mpohler1 <masonpohler@gmail.com>
2020-03-31 17:21:59 -0500

    Added static data for AboutMe as const objects in src/resources

---
+++ src/resources/AboutMe.js
@@ -0,0 +1,39 @@
+export const aboutMeTitle = "About Me";
+
+export const summary = {
+    title: "Summary",
+    content: (
+        "I am a recent college graduate seeking a position as a Junior " +
+        "Software Developer. During my nine-month internship, I worked " +
+        "with a team of full-stack developers who mainly focused attention " +
+        "on a web user portal, an iOS user portal, and the company's " +
+        "internal configuration software. I have gained both frontend and " +
+        "backend skills and have learned a variety of languages and frameworks."
+    )
+};
+
+export const education = {
+    title: "Education",
+    school: "University of Texas. San Antonio, TX",
+    graduation: "Dec 2019",
+    degree: "Bachelor of Science in Computer Science",
+    concentration: "Concentration in Software Engineering"
+};
+
+export const workExperience = {
+    title: "Work Experience",
+    position: "Software Developer Intern",
+    employer: "WellAware Holdings, Inc",
+    startDate: "Sep 2017",
+    endDate: "Jun 2018",
+    content: [
+        "Worked with a team of 4-6 people over a period of nine months",
+        "Used the SCRUM development cycle",
+        "Regularly fixed bugs and added features for the user portal and internal configuration software",
+        "Implemented sorting and filtering of customer assets in user portal",
+        "Imported historical data from new customers into the database",
+        "Created a tool for uploading customer images in bulk",
+        "Created custom reports for customers",
+        "Tested and validated user portal application for iOS"
+    ]
+};
