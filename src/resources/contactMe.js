68b931c07db2edf7bf9fcc46ce9d24b44bea6f24
mpohler1 <masonpohler@gmail.com>
2020-03-31 22:05:00 -0500

    added contact information to resources

---
+++ src/resources/contactMe.js
@@ -0,0 +1,6 @@
+export const contactInfo = {
+    name: "Mason Pohler",
+    email: "masonpohler@gmail.com",
+    linkedIn: "https://www.linkedin.com/in/mason-pohler-2aa626199/",
+    gitHub: "https://github.com/mpohler1"
+};
