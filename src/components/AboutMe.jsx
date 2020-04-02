ee19cb16364feef787fb8e97bfd7021a95b202b2
mpohler1 <masonpohler@gmail.com>
2020-04-02 02:57:37 -0500

    changed light to white

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,42 +5,38 @@ class AboutMe extends Component {
 
     render() {
         return (
-            <div className="container-fluid page bg-light center">
-                <div className="container">
-                    <div className="row center pt-3">
-                        <h1 className="h1 text-center">{aboutMeTitle}</h1>
-                    </div>
-                    <div className="row pb-3">
-                        <div className="container center">
-                            <div className="row">
-                                <div className="col">
-                                    {background.map(item => (
-                                        <div className="row d-block content">
-                                            <h3 className="h3">{item.title}</h3>
-                                            {item.headline ? <h5 className="h5">{item.headline}</h5>:null}
-                                            <p>
-                                                {item.content}
-                                                {item.subContent ?
-                                                    <React.Fragment>
-                                                        <br/> {item.subContent}
-                                                    </React.Fragment>
-                                                :null}
-                                            </p>
-                                        </div>
-                                    ))}
-                                    <div className="row d-block">
-                                        <h3 className="h3">{skills.title}</h3>
-                                        <ul className="list-group">
-                                            {skills.content.map(skill => (
-                                                <li className="list-group-item">
-                                                    <h5 className="h5">{skill.name}</h5>
-                                                    <ul className="list-inline">
-                                                        {skill.list.map(tool => <li className="list-inline-item">{tool}</li>)}
-                                                    </ul>
-                                                </li>
-                                            ))}
-                                        </ul>
+            <div className="container-fluid page bg-white center p-0 m-0">
+                <div className="container py-4 m-0">
+                    <h1 className="h1 text-center">{aboutMeTitle}</h1>
+                    <div className="container center">
+                        <div className="row">
+                            <div className="col">
+                                {background.map(item => (
+                                    <div className="row d-block content">
+                                        <h3 className="h3">{item.title}</h3>
+                                        {item.headline ? <h5 className="h5">{item.headline}</h5>:null}
+                                        <p>
+                                            {item.content}
+                                            {item.subContent ?
+                                                <React.Fragment>
+                                                    <br/> {item.subContent}
+                                                </React.Fragment>
+                                            :null}
+                                        </p>
                                     </div>
+                                ))}
+                                <div className="row d-block">
+                                    <h3 className="h3">{skills.title}</h3>
+                                    <ul className="list-group">
+                                        {skills.content.map(skill => (
+                                            <li className="list-group-item">
+                                                <h5 className="h5">{skill.name}</h5>
+                                                <ul className="list-inline">
+                                                    {skill.list.map(tool => <li className="list-inline-item">{tool}</li>)}
+                                                </ul>
+                                            </li>
+                                        ))}
+                                    </ul>
                                 </div>
                             </div>
                         </div>
