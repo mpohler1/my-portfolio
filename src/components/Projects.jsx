3fd5f62999e6460c73dfba83a0bdb6b02d962036
mpohler1 <masonpohler@gmail.com>
2020-04-03 03:21:16 -0500

    improved image grid

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,16 +5,18 @@ class Projects extends Component {
 
     render() {
         return (
-            <div className="container-fluid bg-white page center">
-                <div className="row content">
+            <div className="container-fluid bg-white center">
+                <div className="row content m-2">
                     <h1 className="h1">{projects.title}</h1>
                 </div>
                 <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 img-grid">
                     {projects.content.map(project => (
-                        <div className="col m-0 p-0 center">
-                            <img className="img-fluid"
+                        <div className="col p-0 center">
+                            <div className="mb-2 mx-2 mx-md-1 card center">
+                                <img className="card-img-top"
                                  src={project.preview}
                                  alt={project.name}/>
+                            </div>
                         </div>
                     ))}
                 </div>
