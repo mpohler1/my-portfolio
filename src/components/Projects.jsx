e1777cf7675c46678feb013dfb7f5620f19066d6
mpohler1 <masonpohler@gmail.com>
2020-04-03 01:35:11 -0500

    created image grid

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,23 @@
 import React, {Component} from "react";
+import {projects} from "../resources/projects";
 
 class Projects extends Component {
 
     render() {
         return (
             <div className="container-fluid bg-white page center">
-
+                <div className="row content">
+                    <h1 className="h1">{projects.title}</h1>
+                </div>
+                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 img-grid">
+                    {projects.content.map(project => (
+                        <div className="col m-0 p-0 center">
+                            <img className="img-fluid"
+                                 src={project.preview}
+                                 alt={project.name}/>
+                        </div>
+                    ))}
+                </div>
             </div>
         )
     }
