3f6a82afb26ac765575b9869f392877e6b565a8f
mpohler1 <masonpohler@gmail.com>
2020-04-03 23:30:19 -0500

    added opacity of headline based on scroll. Headline fades just before navbar overlaps it.

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -1,14 +1,20 @@
 import React, {Component} from "react";
 import {headline} from "../resources/headline";
+import {connect} from "react-redux";
 
 class Headline extends Component {
 
     render() {
         return (
-            <div className="container-fluid page center my-auto">
+            <div className="container-fluid page center my-auto"
+                 ref={div => div && div.setAttribute(
+                     "style", "opacity: " + this.props.opacity
+                 )}>
                 <div className="row">
                     <div className="col-12">
-                        <h1 className="display-4 text-white text-left">{headline}</h1>
+                        <h1 className="display-4 text-white text-left">
+                            {headline}
+                        </h1>
                     </div>
                 </div>
             </div>
@@ -16,4 +22,10 @@ class Headline extends Component {
     }
 }
 
-export default Headline;
+const mapStateToProps = state => {
+    return {
+        opacity: 1 - (state.navbar.backgroundOpacity * 2).toFixed(2)
+    }
+};
+
+export default connect(mapStateToProps)(Headline);
