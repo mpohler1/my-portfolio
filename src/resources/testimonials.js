ae92fa0dee0be1373f87620eeda6e20fc353597b
mpohler1 <masonpohler@gmail.com>
2020-06-22 18:47:31 -0500

    added testimonials from my mentors

--- src/resources/testimonials.js
+++ src/resources/testimonials.js
@@ -2,19 +2,26 @@ export const testimonials = {
     title: "Testimonials",
     content: [
         {
-            name: "vel pretium",
-            title: "posuere urna nec",
-            quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium lectus quam id leo in vitae turpis. Ultrices vitae auctor eu augue ut lectus."
+            name: "Jonathan Burgin",
+            title: "PhD",
+            quote: "I had the pleasure  of Mason reporting to me while he was an intern at WellAware Holdings Inc. " +
+                "Mason showed great eagerness, initiative and flexibility with his assignments. One of Mason's " +
+                "attributes is a teachable spirit, which made it a blessing to work with him and his education. We " +
+                "did not give him 'intern tasks', but treated him as a regular college hire employee with " +
+                "commensurate task difficulties. He accomplished all assignments to him with a positive attitude and " +
+                "willingness to learn that indicates he would be a positive asset to any team lucky enough to hire him."
         },
         {
-            name: "pretium vulputate",
-            title: "vestibulum rhoncus est",
-            quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum iaculis eu non diam phasellus vestibulum lorem. Nibh venenatis cras sed felis."
-        },
-        {
-            name: "volutpat diam",
-            title: "sit amet facilisis",
-            quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna. Pellentesque pulvinar pellentesque habitant morbi tristique."
+            name: "Alan Erwin",
+            title: "Software Developer III",
+            quote: "I have had the opportunity to mentor many people throughout my career. Mason was one of the few " +
+                "that truly stood out. During his tenure as my intern, he always showed an appetite for learning and " +
+                "a work ethic that would follow through. An example of this was, during one of the projects he was " +
+                "doing he got stuck. He spent a day working on a few different solutions to figure out what was the " +
+                "best approach. Once he had learned what needed to be done, and the approach to take, he built a " +
+                "demo and brought it to me. This showed me that he had the willingness to learn,  the work ethic to " +
+                "take on the unknown, and the confidence to own his decisions. I hope in the future to get to work " +
+                "with him again."
         }
     ]
 };
