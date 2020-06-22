2b7235c92e8ef380c597659cb46dc6ade7fbc550
mpohler1 <masonpohler@gmail.com>
2020-06-22 18:52:12 -0500

    added api call to Projects API in ProjectsGrid

--- src/components/ProjectGrid.jsx
+++ src/components/ProjectGrid.jsx
@@ -1,17 +1,34 @@
 import React, {Component} from "react";
-import {projects} from "../resources/projects";
+import {connect} from "react-redux";
+import {fetchProjectsFailure, fetchProjectsRequest, fetchProjectsSuccess} from "../actions/actions";
+import {fetchProjects} from "../service/projectsService";
 
 class ProjectGrid extends Component{
+
+    determineProjectList() {
+        this.props.fetchProjectsRequest();
+        fetchProjects().then(([response, json]) => {
+            if (response.status === 200) {
+                this.props.fetchProjectsSuccess(json);
+            } else {
+                this.props.fetchProjectsFailure();
+            }
+        });
+    }
+
+    componentDidMount() {
+        this.determineProjectList();
+    }
 
     render() {
         return (
             <div className="container">
                 <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 img-grid">
-                    {projects.content.map(project => (
+                    {this.props.projects.map(project => (
                         <div className="col p-0 center">
                             <div className="mb-2 mx-2 my-sm-1 mx-sm-1 center">
                                 <img className="card-img-top rounded"
-                                     src={project.preview}
+                                     src={project.previewURL}
                                      alt={project.name}/>
                             </div>
                         </div>
@@ -22,4 +39,14 @@ class ProjectGrid extends Component{
     }
 }
 
-export default ProjectGrid;
+const mapStateToProps = state => {
+    return {
+        projects: state.projects.projects
+    }
+};
+
+export default connect(mapStateToProps, {
+    fetchProjectsRequest,
+    fetchProjectsSuccess,
+    fetchProjectsFailure
+})(ProjectGrid);
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import {projects} from "../resources/projects";
 import ProjectGrid from "./ProjectGrid";
 
 class Projects extends Component {
@@ -8,7 +7,7 @@ class Projects extends Component {
         return (
             <div className="container-fluid page bg-white center" id="projects">
                 <div className="row content mt-5 mb-5">
-                    <h1 className="h1">{projects.title}</h1>
+                    <h1 className="h1">Projects</h1>
                 </div>
                 <div className="row mb-5 mt-5">
                     <p>Here is some of my recent work.</p>
