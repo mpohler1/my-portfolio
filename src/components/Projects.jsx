6218942f87bc52161ba5ce53298a6093b7f8ae77
mpohler1 <masonpohler@gmail.com>
2020-06-24 00:57:53 -0500

    improved spacing of Projects and Testimonials for mobile

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,10 +8,10 @@ class Projects extends Component {
     render() {
         return (
             <div className="container-fluid page bg-white text-dark center" id="projects">
-                <div className="row my-auto">
+                <div className="row my-3 my-sm-auto">
                     <h4 className="display-4 font-weight-light">My Projects</h4>
                 </div>
-                <div className="row mb-auto w-100 center">
+                <div className="row mb-3 mb-sm-auto w-100 center">
                     <ProjectFilter/>
                     <ProjectGrid/>
                     <ProjectModal/>
